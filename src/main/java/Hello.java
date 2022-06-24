08d7db7d460208488d80912855ca748cbe19f5fb
SoonheonKwon <shkwon93@nate.com>
2022-06-24 12:00:53 +0900

    Hello 클래스 수정하기

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello01");
         System.out.println("hello02");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("안녕하세요.");
+        }
     }
 }
